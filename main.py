30220621a257fce7598ef91b73306c8cad4126f9
Dipayan Ghosh <krishnadipayan108@gmail.com>
2023-09-24 19:14:51 +0530

    🚀

---
+++ main.py
@@ -0,0 +1,22 @@
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+import plotly.express as px 
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator 
+from sklearn.model_selection import train_test_split 
+from sklearn.linear_model import PassiveAggressiveRegressor 
+
+
+data = pd.read_csv("Instagram data.csv", encoding="latin1")
+print(data.head())
+
+data.isnull().sum()
+data = data.dropna()
+data.info()
+
+plt.figure(figsize=(10, 8))
+plt.style.use('fivethirtyeight')
+plt.title("Distribution of Impressions from home")
+sns.distplot(data['From Home'])
+plt.show()
